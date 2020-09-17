f49105622133a7b617dd37797af5dfc3b0fd9dfb
Eunho <eunhokim4023@daum.net>
2020-09-17 17:34:31 +0900

    Desktop css for login finished. commiting before adding tablet css media tag

---
+++ src/Components/Login2/Login.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Login.css';
+const Login = () => {
+    return (
+        <div className = "ToraLogin">
+            <p className = "LoginTitle" id = "firstText">Email Address</p>
+            <input type = "text"></input>
+            <p className = "LoginTitle">Password</p>
+            <input className = "LoginTitle" type = "password"></input><br></br>
+            <button type = "submit"> Log in</button>
+
+            <div className = "LoginText">
+                <p>Sign up for Tora</p>
+                <p>Search Email Address</p>
+            </div>
+
+        </div>
+    );
+};
+
+export default Login;
